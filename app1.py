d742a198511a6f8d1898ca165fd45d06170a99e4
Hardik Parmar <121241956+hardikjp7@users.noreply.github.com>
2024-02-01 12:30:59 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -18,34 +18,34 @@ def main():
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        pregnancies = st.number_input('Pregnancies', value=0)
+        pregnancies = float(st.text_input('Pregnancies', value='0', help='Number of times pregnant'))
 
     with col2:
-        glucose = st.number_input('Glucose', value=0)
+        glucose = float(st.text_input('Glucose', value='0', help='Plasma glucose concentration'))
 
     with col3:
-        blood_pressure = st.number_input('Blood Pressure', value=0)
+        blood_pressure = float(st.text_input('Blood Pressure', value='0', help='Diastolic blood pressure'))
 
     col4, col5, col6 = st.columns(3)
 
     with col4:
-        skin_thickness = st.number_input('Skin Thickness', value=0)
+        skin_thickness = float(st.text_input('Skin Thickness', value='0', help='Triceps skin fold thickness'))
 
     with col5:
-        insulin = st.number_input('Insulin', value=0)
+        insulin = float(st.text_input('Insulin', value='0', help='Insulin levels'))
 
     with col6:
-        bmi = st.number_input('BMI', value=0)
+        bmi = float(st.text_input('BMI', value='0', help='Body Mass Index (BMI)'))
 
     col7, col8, col9 = st.columns(3)
 
     with col7:
-        diabetes_pedigree_function = st.number_input('Diabetes Pedigree Function', value=0)
+        diabetes_pedigree_function = float(st.text_input('Diabetes Pedigree Function', value='0', help='Assign a value of 0 if it is not available.'))
 
     with col8:
-        age = st.number_input('Age', value=0)
+        age = float(st.text_input('Age', value='0', help='Age of the patient'))
 
-# Predict button
+    # Predict button
     if st.button('Predict'):
         new_data = scaler.transform([[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree_function, age]])
         prediction = model.predict(new_data)
